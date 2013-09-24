bde5bb53441f6f8a00e562b5e4d766557fbc7de3
oliverswitzer <oliverswitzer@gmail.com>
2013-09-24 15:07:55 -0400

    created a sort_tasks and print_task function for the Task class

--- object_oriented_proj/task_manager.py
+++ object_oriented_proj/task_manager.py
@@ -1,11 +1,27 @@
-
+import operator
 
 class Task:
 
 	task_dict = {}
 
-	def addtask(self, Task, time):
-		self.task_dict[Task] = time
-		if Task == None or time == None:
+	def addtask(self, Task, priority):
+		self.task_dict[Task] = int(priority)
+		if Task == "" or priority == "":
 			raise SyntaxError("Please input a task")
 
+	def sort_tasks(self, task_dict):
+
+		x = {1: 2, 3: 4, 4:3, 2:1, 0:0}
+		sorted_tasks = sorted(task_dict.iteritems(), key=operator.itemgetter(1), reverse = True)
+		return sorted_tasks
+
+	
+	def printtasks(self):
+		for k, v in self.task_dict.iteritems():
+			print k,
+			print " ",
+			print v.rjust(20, '*')
+
+if __name__ == "__main__":
+	task_dict  = {'feed dog':'3', 'eat something':'5', 'do math homework':'1'}
+	print sort_tasks(task_dict)
