b46cd671ef73252d1defeee8b3fc8a1e1e5ed503
oliverswitzer <oliverswitzer@gmail.com>
2013-09-26 13:54:21 -0400

    Revert 1c830ee..18e21f0
    This rolls back to commit 1c830eeee8c489103f9cdda327718b94f8e86549.

--- object_oriented_proj/task_manager_test.py
+++ object_oriented_proj/task_manager_test.py
@@ -2,18 +2,20 @@
 
 
 import unittest
-from task_manager import Task
+from task_manager import TaskList, NewTask 
 
 
-class TaskManagerTest(unittest.TestCase):
+class NewTaskTest(unittest.TestCase):
 
 	def setUp(self):
-		self.task = Task()
+		self.task = NewTask()
 
-	def test_that_user_cant_enter_empty_task(self):
-		self.assertRaises(SyntaxError, self.task.addtask, "", "10") 
-	def test_that_task_isnt_numerical_value(self):
-		self.assertRaises(ValueError, self.task.addtask, "10", "4")
-	def test_that_user_enters_a_priority_value_to_complete_task(self):
-		self.assertRaises(SyntaxError, self.task.addtask, "Feed dog", "")
+	# def test_that_user_cant_enter_empty_task(self):
+	# 	self.assertRaises(SyntaxError, self.task.addtask, "", "10") 
+	# def test_that_task_isnt_numerical_value(self):
+	# 	self.assertRaises(ValueError, self.task.addtask, "10", "4")
+	# def test_that_user_enters_a_priority_value_to_complete_task(self):
+	# 	self.assertRaises(SyntaxError, self.task.addtask, "Feed dog", "")
+	def test_that_user_enters_a_priority_value_betwn_1_and_10(self):
+		self.assertRaises(ValueError, self.task.__init__)
 	
